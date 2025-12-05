ba02e477afdbe5463f8e4f5591d4d48cbd05394c
Arno Bock <arno.bock@hotmail.de>
2025-12-05 19:01:54 +0100

    [Prototype] clean up main branch + add 1st presentation

--- db/connection.py
+++
@@ -1,9 +0,0 @@
-import psycopg
-
-
-def get_connection():
-    # For now, we hardcode the local dev DB.
-    # Later we can move this to environment variables.
-    return psycopg.connect(
-        "postgresql://middleware_user:middleware_pwd@localhost:5434/middleware_genai"
-    )
--- test_search.py
+++
@@ -1,7 +0,0 @@
-from embedding_manager import search_similar
-
-if __name__ == "__main__":
-    results = search_similar("Middleware", top_k=5)
-    print("Search results:")
-    for r in results:
-        print(r)
